74e5baf245a9d934920368cfd8623d2cab4a61c9
Estecka <estecka@galacsys.com>
2025-12-31 15:55:12 +0100

    Identity changes

---
+++ src/main/java/fr/estecka/untamedoffsprings/UntamedOffspringsMod.java
@@ -0,0 +1,17 @@
+package fr.estecka.untamedoffsprings;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UntamedOffspringsMod
+implements ModInitializer
+{
+	static public final String MODID = "untamed-offsprings";
+	static public final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
